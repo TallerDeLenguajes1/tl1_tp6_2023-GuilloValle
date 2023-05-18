4b6f25c4ce63c7c90b93be2bd888116fc6fd7376
GuilloValle <guillo.evsa@gmail.com>
2023-05-18 18:43:33 -0300

    prueba branch

--- parctico6/Program.cs
+++ parctico6/Program.cs
@@ -12,3 +12,28 @@ b = a;
 Console.WriteLine("Valor de a:"+a);
 
 Console.WriteLine("valor de b:"+b);
+
+float num1 = 0;
+bool anda = false;
+string? numString = "";
+
+while (!anda)
+{
+    Console.WriteLine("Ingrese un numero valido:");
+    numString = Console.ReadLine();
+    anda = float.TryParse(numString,out num1);
+    if (!anda)
+    {
+        Console.WriteLine(numString + " no es un numero valido");
+    }
+}
+
+Console.WriteLine(numString + " es valido");
+
+//invertir numero
+
+Console.WriteLine(numString + " es valido");
+Console.WriteLine(numString + " es valido");
+Console.WriteLine(numString + " es valido");
+Console.WriteLine(numString + " es valido");
+
