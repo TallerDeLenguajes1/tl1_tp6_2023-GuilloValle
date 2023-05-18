f89aaaca07beb636b5cbce5ae941f3467612d783
GuilloValle <guillo.evsa@gmail.com>
2023-05-18 19:59:19 -0300

    falta que pregunte de nuevo

--- parctico6/Program.cs
+++ parctico6/Program.cs
@@ -1,7 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int a;
+void calculadoraV1(){
+
+    string? opcion;
+    bool casteo;
+    int num,num2,num3,resultado;
+
+    System.Console.WriteLine("---ELIJA LA OPCION---");
+    System.Console.WriteLine("0: SUMA");
+    System.Console.WriteLine("1: RESTA");
+    System.Console.WriteLine("2: MULTIPLICAR");
+    System.Console.WriteLine("3: DIVIDIR");
+    opcion = Console.ReadLine();
+    casteo = int.TryParse(opcion,out num);
+
+    if (casteo)
+    {
+        System.Console.Write("Ingrese el primer numero:");
+        opcion = Console.ReadLine();
+        casteo = int.TryParse(opcion,out num2);
+        if (casteo)
+        {
+        System.Console.Write("Ingrese el Segundo numero:");
+        opcion = Console.ReadLine();
+        casteo = int.TryParse(opcion,out num3);
+            if (casteo)
+            {
+                switch (num)
+                {
+                    case 0: resultado = num2 + num3;
+                            System.Console.WriteLine(resultado);
+                    break;
+                    case 1: resultado = num2 - num3;
+                            System.Console.WriteLine(resultado);
+                    break; 
+                    case 2: resultado = num2 * num3;
+                            System.Console.WriteLine(resultado);
+                    break; 
+                    case 3: resultado = num2 / num3;
+                            System.Console.WriteLine(resultado);
+                    break;  
+                    default: System.Console.WriteLine("no eligio una opcion valida");
+                    break;
+                }
+            }
+        }
+    }
+
+    
+}
+
+calculadoraV1();
+
+/*int a;
 
 int b;
 
@@ -26,14 +78,7 @@ while (!anda)
     {
         Console.WriteLine(numString + " no es un numero valido");
     }
-}
+}*/
 
-Console.WriteLine(numString + " es valido");
 
-//invertir numero
-
-Console.WriteLine(numString + " es valido");
-Console.WriteLine(numString + " es valido");
-Console.WriteLine(numString + " es valido");
-Console.WriteLine(numString + " es valido");
 
