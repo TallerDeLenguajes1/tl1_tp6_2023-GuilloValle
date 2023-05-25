7f5e1ad2e1832bebfea8db4877a48eec38da612d
GuilloValle <guillo.evsa@gmail.com>
2023-05-25 18:45:06 -0300

    que paso

--- parctico6/Program.cs
+++ parctico6/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 
 int a;
 
@@ -40,7 +40,65 @@ while (!anda)
             }
         }
     }
+}*/
+
+
+
+/* PUNTO 4 --------------------------------------------------------------------------*/
+
+
+
+
+Console.WriteLine("Ingrese la primera cadena:");
+string cadena1 = Console.ReadLine();
+int longitud = cadena1.Length;
+Console.WriteLine("La longitud de la primer cadena es: " + longitud);
+
+Console.WriteLine("Ingrese la segunda cadena:");
+string cadena2 = Console.ReadLine();
+
+string concatenacion = cadena1 + cadena2;
+
+Console.WriteLine("La concatenación de las dos cadenas es: " + concatenacion);
+
+string subcadena1 = cadena1.Substring(0, 4); 
+Console.WriteLine("la subcadena de cadena 1 es: "+ subcadena1);
+
+
+foreach (char caracter in cadena1)
+{
+    Console.WriteLine(caracter);
 }
 
 
+Console.WriteLine("Ingrese la palabra a buscar en cadena 2:");
+string? palabra = Console.ReadLine();
+
+bool contienePalabra = cadena2.Contains(palabra);
+
+if (contienePalabra)
+{
+    Console.WriteLine("La palabra '" + palabra + "' se encuentra en la cadena 2.");
+}
+else
+{
+    Console.WriteLine("La palabra '" + palabra + "' no se encuentra en la cadena 2.");
+}
+
+
+string cadenaMayusculas = cadena1.ToUpper();
+string cadenaMinusculas = cadena1.ToLower();
+
+Console.WriteLine("Cadena 1 en mayúsculas: " + cadenaMayusculas);
+Console.WriteLine("Cadena 1 en minúsculas: " + cadenaMinusculas);
+
+string cadena3 = "Hola, Mundo, Perro, Chau";
+char delimitador = ','; // Utilizamos una coma como delimitador
+
+string[] subcadenas = cadena3.Split(delimitador);
+
+foreach (string subcadena in subcadenas)
+{
+    Console.WriteLine(subcadena);
+}
 
